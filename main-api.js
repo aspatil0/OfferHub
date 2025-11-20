80595ae67c80b89029d58e67155ca4126bfc2804
ADITYARAJ PATIL <119296947+aspatil0@users.noreply.github.com>
2025-11-20 23:13:55 +0530

    Update main-api.js

--- main-api.js
+++ main-api.js
@@ -1,1 +1,1 @@
-// url = f"https://www.google.com/search?q={search_query}+site:amazon.in+OR+site:flipkart.com&tbm=shop"
+url = f"https://www.google.com/search?q={search_query}+site:amazon.in+OR+site:flipkart.com&tbm=shop"
